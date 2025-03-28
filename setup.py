db3088558d9e9cd650e1c45d4e0e91e8d38107b2
Alan <alanunni3@gmail.com>
2025-03-28 22:22:46 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+
+
+def get_requirements(file_path:str)->list:
+    """
+    This function returns a list of requirements from the given file path.
+    """
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+    
+setup(
+    name="Network-security-project",
+    version="0.0.1",
+    author='Alan Jose',
+    author_email='alanunni3@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt'),
+    description="A Network security project"
+)
